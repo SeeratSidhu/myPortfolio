4983eab374ee8f1aead6ea0108202a6d1e381123
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-08-14 20:15:53 -0400

    fixed the alignment issue for the images

--- assets/logic.js
+++ assets/logic.js
@@ -3,14 +3,18 @@ window.onload = function () {
 
 
     var modal = document.getElementById("things");
-    var img = document.getElementsByClassName("image1");
+    var img = document.getElementsByClassName("image-container");
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
-    img.onclick = function () {
+    
+    $(".image-container").on("click", function(){
         modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
+    });
+    // img.onclick = function () {
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    // }
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
